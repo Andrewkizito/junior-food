da8522a087446ec38d93f4b258a5213d38a01d9b
AndrewKizito <AndrewKizito54@gmail.com>
2022-11-07 12:03:27 +0300

    Added Backend

--- src/pages/Home/sections/Welcome.tsx
+++ src/pages/Home/sections/Welcome.tsx
@@ -1,8 +1,45 @@
+//Importing helper functions
+import { Grid } from "@mui/material";
+import classNames from "classnames";
+import { welcome_slots } from "../../../utils/data";
+
+//Importing core components
+
 //Importing styles
 import styles from "../styles.module.scss";
 
 const Welcome = () => {
-  return <div className={styles.Welcome}></div>;
+  return (
+    <div className={classNames(styles.Welcome, "center")}>
+      <div className={classNames(styles.content, "center-row")}>
+        <img
+          className={styles.Image}
+          src="/images/burger-banner.png"
+          alt="burger"
+        />
+        <div className={styles.info}>
+          <h1>
+            HEALTHY DELICIOUS MEALS <span>AT AFFORDABLE</span> PRICES.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
+            dictum quam, a elementum magna. Donec non interdum tortor. Ut cursus
+            tristique metus. Maecenas ultrices venenatis neque id pellentesque.{" "}
+          </p>
+          <Grid container spacing={3}>
+            {welcome_slots.map((item) => (
+              <Grid item xs={12} sm={4}>
+                <div className={classNames(styles.item, "center")}>
+                  <div dangerouslySetInnerHTML={{ __html: item.icon }} />
+                  <h4>{item.title}</h4>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
---
+++ src/utils/data.ts
@@ -0,0 +1,19 @@
+type slot = {
+  title: string;
+  icon: string;
+};
+
+export const welcome_slots: slot[] = [
+  {
+    title: "Healthy Meals",
+    icon: "<i class='icofont-food-basket'></i>",
+  },
+  {
+    title: "Fast Delivery",
+    icon: "<i class='icofont-fast-delivery'></i>",
+  },
+  {
+    title: "Highly Affordable",
+    icon: "<i class='icofont-money-bag'></i>",
+  },
+];
