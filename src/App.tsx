8efa424ce4020e014bb305c87987aa13a9158303
AndrewKizito <AndrewKizito54@gmail.com>
2022-11-07 10:13:58 +0300

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Root from './Root'
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  )
+}
+
+export default App
